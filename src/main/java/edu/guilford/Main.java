2ad0777baa135837e5a0db218aa70a6ede139967
hchueb <hchueb@guilford.edu>
2024-09-19 14:15:37 -0400

    Hello 👋

--- src/main/java/edu/guilford/Main.java
+++ src/main/java/edu/guilford/Main.java
@@ -1,11 +1,27 @@
 package edu.guilford;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
 
-        // default for loop, prints loop variable as it counts up
-        for (int i = 0; i < 10; i++) {
-            System.out.println("loop variable i = " + i);
+        System.out.println("Enter the number of sides for the dice:");
+        int sides = scan.nextInt();
+
+        System.out.println("Enter the number of dice to roll:");
+        int number = scan.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i < number; i++) {
+            int diceRoll = rand.nextInt(sides) + 1;
+            sum += diceRoll;
+            // System.out.println("Sum of " + (i + 1) + "d" + sides + " = " + sum);
         }
+
+        System.out.println(number + "d" + sides + " = " + sum);
     }
 }
